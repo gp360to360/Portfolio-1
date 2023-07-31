c1e0df3ed62aac286361b44fee585af1e461514f
gyanendra prajapati <99497106+gp360to360@users.noreply.github.com>
2023-07-31 20:28:04 +0530

    Update AppWrap.js

--- frotend_react/src/wrapper/AppWrap.js
+++ frotend_react/src/wrapper/AppWrap.js
@@ -9,8 +9,8 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2020 GYANENDRA</p>
-          <p className="p-text">All rights reserved</p>
+          {/* <p className="p-text">@2020 GYANENDRA</p> */}
+          {/* <p className="p-text">All rights reserved</p> */}
         </div>
       </div>
       <NavigationDots active={idName} />
